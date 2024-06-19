42f4e47d755b3491b599804f10edf8e60509be8d
koji.iwase <koji.iwase@prtimes.co.jp>
2024-06-19 10:44:10 +0900

    CustomRequestクラス

---
+++ src/app/Http/Requests/UpdateArticleRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // 必要に応じて認可ロジックを追加します。
+        // ここでは常にtrueを返すように設定します。
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'body' => 'required|string|max:5000',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'タイトルは必須です。',
+            'title.max' => 'タイトルは255文字以内で入力してください。',
+            'body.required' => '本文は必須です。',
+            'body.max' => '本文は5000文字以内で入力してください。',
+        ];
+    }
+}
