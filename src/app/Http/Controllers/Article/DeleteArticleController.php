99a82ba6918a845ee0f708ce81f1938106f0a877
koji.iwase <koji.iwase@prtimes.co.jp>
2024-06-26 15:11:34 +0900

    deleteの変更

--- src/app/Http/Controllers/Article/DeleteArticleController.php
+++ src/app/Http/Controllers/Article/DeleteArticleController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\Article;
 
 use App\Http\Controllers\Controller;
 use App\Models\Article;
+use Illuminate\Support\Facades\Auth;
 
 class DeleteArticleController extends Controller
 {
     public function __invoke(Article $article)
     {
+        if ($article->user_id !== Auth::id()) {
+            return redirect()->route('home')->with('error', '他のユーザーの投稿は削除できません');
+        }
         $article->delete();
         return redirect()->route('home')->with('success', 'Article deleted successfully.');
     }
